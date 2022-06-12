688d1f566a9a65465e94164bc4d35748f50d3764
Nahuel Genchi <nagen01@gmail.com>
2022-06-12 01:16:12 -0300

    Keeping appearance preference with localStorage

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
 const header = document.querySelector(".header");
 const main = document.querySelector(".mainIndex");
 const footer = document.querySelector(".footer");
-
 const appearance = document.querySelector("#appearance");
-appearance.lightMode = true;
 
 function lightMode() {
-    if (appearance.lightMode === false) {
-        header.classList.add("header-dark");
-        main.classList.add("main-dark");
-        footer.classList.add("footer-dark");
-        header.classList.remove("header-light");
-        main.classList.remove("main-light");
-        footer.classList.remove("footer-light");
-        appearance.lightMode = true;
+    header.classList.remove("header-dark");
+    main.classList.remove("main-dark");
+    footer.classList.remove("footer-dark");
+    header.classList.add("header-light");
+    main.classList.add("main-light");
+    footer.classList.add("footer-light");
+};
+function darkMode() {
+    header.classList.remove("header-light");
+    main.classList.remove("main-light");
+    footer.classList.remove("footer-light");
+    header.classList.add("header-dark");
+    main.classList.add("main-dark");
+    footer.classList.add("footer-dark");
+};
+function appearanceFunction() {
+    if (localStorage.getItem("appearance") == "dark") {
+        darkMode();
     } else {
-        header.classList.add("header-light");
-        main.classList.add("main-light");
-        footer.classList.add("footer-light");
-        header.classList.remove("header-dark");
-        main.classList.remove("main-dark");
-        footer.classList.remove("footer-dark");
-        appearance.lightMode = false;
+        lightMode();
     };
 };
+appearanceFunction();
 
-appearance.addEventListener("click", lightMode);
+function appearanceChange() {
+    if (localStorage.getItem("appearance") == "dark") {
+        localStorage.setItem("appearance", "light");
+        appearanceFunction();
+    } else {
+        localStorage.setItem("appearance", "dark");
+        appearanceFunction();
+    };
+    console.log("hi");
+};
 
+
+
+appearance.addEventListener("click", appearanceChange);
